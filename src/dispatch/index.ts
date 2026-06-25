764bf93b476c6629a03407533c4074ed95755a8d
EricMoin <ericmoin@deepseek.com>
2026-06-25 16:52:01 +0800

    refactor(dispatch): update tools and index exports

--- src/dispatch/index.ts
+++ src/dispatch/index.ts
@@ -1,6 +1,9 @@
 export { DispatchManager } from "./manager.js";
 export { ConcurrencyManager } from "./concurrency.js";
-export { SessionPoller } from "./poller.js";
+export { GlobalPoller } from "./global-poller.js";
+export { GlobalPoller as SessionPoller } from "./global-poller.js"; // deprecated alias
+export { SessionMonitor } from "./session-monitor.js";
+export { detectCompletion } from "./completion-detector.js";
 export { buildNotificationText, notifyParent } from "./notification.js";
 export { createDispatchTool, createDispatchOutputTool, createDispatchCancelTool } from "./tools.js";
 export type * from "./types.js";
